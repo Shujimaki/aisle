41121b611d4f9e0bc6686539497e0706038adbc4
Brian Metrillo <140519835+Shujimaki@users.noreply.github.com>
2025-10-27 15:56:46 +0800

    Add testing lines for app.py

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
+@app.before_request
+def before_request():
+    print("What is love?")
+
+@app.after_request
+def after_request(response):
+    print(response)
+    return response
+
+@app.route('/')
+def home():
+    return "Hello World"
+
+# add for future login (for chatting with ai)
+@app.route('/login')
+def login():
+    return "login here"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
