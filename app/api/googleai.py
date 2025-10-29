8575dd095c0f44a68598ce7bd1e8b4e0d1e453ef
Brian Metrillo <140519835+Shujimaki@users.noreply.github.com>
2025-10-30 00:06:17 +0800

    Add exception handling to summary generation

--- app/api/googleai.py
+++ app/api/googleai.py
@@ -1,39 +1,48 @@
 import os
 import redis
 from google import genai
-from google.genai import types
+from google.genai import types, errors
 from dotenv import load_dotenv
 from .caching import Cache
 
 
-def generate_summary(data: dict) -> str:
+def generate_summary(data: dict) -> dict:
 
     env_path = os.path.join(os.path.dirname(__file__), '../../.env')
     load_dotenv(dotenv_path=env_path)
     api_key = os.getenv("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(
-        model = "gemini-2.5-flash",
-        config=types.GenerateContentConfig(
-            system_instruction="You are a super AI agent that analyzes and translates PHIVOLCS' earthquake details " \
-            "into simple, friendly, consumable and easy-to-digest information for Filipinos. You are able to give the" \
-            "reports primarily in English, but also in distinct Filipino native languages, per their preferences"
-        ),
-        contents=
-        "In a 4-sentence paragraph, summarize of the earthquake detail reports, along with" \
-        "2 simple tips on earthquake safety for affected regions (include in the paragraph)." \
-        "Make sure the tips are appropriate for the situation and severity." \
-        "Earthquake Information:" \
-        f"Date and Time: {data["date_time"]}"
-        f"Latitude: {data['latitude']}"
-        f"Longitude: {data['longitude']}"
-        f"Depth: {data['depth']}"
-        f"Magnitude: {data['magnitude']}"
-        f"Location: {data['location']}"
-    )
+    try:
+        response = client.models.generate_content(
+            model = "gemini-2.5-flash",
+            config=types.GenerateContentConfig(
+                system_instruction="You are a super AI agent that analyzes and translates PHIVOLCS' earthquake details " \
+                "into simple, friendly, consumable and easy-to-digest information for Filipinos. You are able to give the" \
+                "reports primarily in English, but also in distinct Filipino native languages, per their preferences"
+            ),
+            contents=
+            "In a 4-sentence paragraph, summarize of the earthquake detail reports, along with" \
+            "2 simple tips on earthquake safety for affected regions (include in the paragraph)." \
+            "Make sure the tips are appropriate for the situation and severity." \
+            "Earthquake Information:" \
+            f"Date and Time: {data["date_time"]}"
+            f"Latitude: {data['latitude']}"
+            f"Longitude: {data['longitude']}"
+            f"Depth: {data['depth']}"
+            f"Magnitude: {data['magnitude']}"
+            f"Location: {data['location']}"
+        )
 
-    return response.text
+        return {
+            "success": True,
+            "data": response.text
+        }
+    except Exception as e:
+        return {
+            "success": False,
+            "error": str(e)
+        }
 
     
 def fetch_summary(earthquake):
@@ -41,7 +50,11 @@ def fetch_summary(earthquake):
     data = cache.get()
     if data is not None:
         return data
-    else:
-        data = generate_summary(earthquake)
-        cache.set(data)
-        return data
+    
+    data = generate_summary(earthquake)
+
+    if data["success"]:
+        cache.set(data["data"])
+        return data["data"]
+
+    return f"Error generating earthquake summary. Please refer to the original bulletin for details. error: {e}"
